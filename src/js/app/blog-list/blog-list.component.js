af1a0861cb3dfaef004c2bdadd2423e1ab5e6776
Aniket Savanand <aniketpsavanand@gmail.com>
2016-10-29 15:20:05 -0700

    order by

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -6,7 +6,7 @@ angular.module('blogList').
         templateUrl: "/templates/blog-list.html",
         controller: function (Post, $location, $routeParams, $rootScope, $scope) {
 
-
+            $scope.order = "title"
             $scope.goToItem = function(post){
                 $rootScope.$apply(function () {
                     $location.path("/blog/" + post.id)
